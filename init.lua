ef6b5de965e841ddae895f9e5863943f70ce573b
Conlin Durbin <conlin.durbin@whatnot.com>
2025-11-19 15:29:42 -0500

    Update personal vim

--- init.lua
+++ init.lua
@@ -1,16 +1,18 @@
 vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
   dev = {
-    path = "~/github/wuz",
+    path = "~/dev/github/wuz",
     patterns = { "wuz", "cantrip.nvim" },
     fallback = false,
   },
@@ -20,23 +22,35 @@ require("lazy").setup({
       dev = true,
       import = "cantrip.plugins",
       opts = {
-        translucent = true,
-        theme = "tokyonight",
+        translucent = false,
+        theme = "lackluster-mint",
       },
-      init = function()
-        vim.g.tokyonight_dark_sidebar = true
-        vim.g.tokyonight_dark_float = true
-      end,
     },
-    -- extra plugins
-    -- { import = "cantrip.plugins.extras.notes" },
-    -- Languange specifc plugins and configs
-    { import = "cantrip.plugins.lsp.languages.nearly" },
-    -- { import = "cantrip.plugins.lsp.languages.markdown" },
-    { import = "cantrip.plugins.lsp.languages.typescript" },
+    -- {
+    --   "wuz/warlock.nvim",
+    --   dev = true,
+    -- },
+    { import = "cantrip.plugins.extras.colors.lackluster" },
+    { import = "cantrip.plugins.completion.blink" },
+    -- Language specific plugins and configs
+    { import = "cantrip.plugins.lsp.languages.lua" },
+    { import = "cantrip.plugins.lsp.languages.javascript" },
+    { import = "cantrip.plugins.lsp.languages.grit" },
+    { import = "cantrip.plugins.lsp.languages.rust" },
+    { import = "cantrip.plugins.lsp.languages.markdown" },
     { import = "cantrip.plugins.lsp.languages.nix" },
-    --    { import = "cantrip.plugins.extras.quicknote" },
-    { import = "cantrip.plugins.extras.tree" },
+    { import = "cantrip.plugins.lsp.languages.python" },
+    { import = "cantrip.plugins.lsp.languages.css" },
+    { import = "cantrip.plugins.lsp.languages.yaml" },
+    -- extra plugins
+    { import = "cantrip.plugins.extras.oil" },
+    { import = "cantrip.plugins.extras.miniharp" },
+    { import = "cantrip.plugins.extras.sessions" },
+    { import = "cantrip.plugins.extras.tailwind" },
+    { import = "cantrip.plugins.extras.nvim_highlight_colors" },
+    { import = "cantrip.plugins.extras.git" },
+    { import = "cantrip.plugins.extras.test" },
+    { import = "cantrip.plugins.inclines.harpoon" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -44,7 +58,7 @@ require("lazy").setup({
     lazy = false,
     version = false, -- always use the latest git commit
   },
-  install = { colorscheme = { "tokyonight" } },
+  install = { colorscheme = { "warlock" } },
   checker = { enabled = false }, -- automatically check for plugin updates
   performance = {
     cache = { enabled = true },
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,68 +1,77 @@
 return {
+  { "pimalaya/himalaya-vim" },
+  { "meznaric/key-analyzer.nvim", opts = {} },
+  { "imsnif/kdl.vim", config = function() end },
+  {
+    "fresh2dev/zellij.vim",
+    -- Pin version to avoid breaking changes.
+    -- tag = '0.3.*',
+    lazy = false,
+    keys = {
+      { "<C-h>", ":ZellijNavigateLeft<CR>" },
+      { "<C-j>", ":ZellijNavigateDown<CR>" },
+      { "<C-k>", ":ZellijNavigateUp<CR>" },
+      { "<C-l>", ":ZellijNavigateRight<CR>" },
+    },
+    init = function()
+      -- Options:
+      -- vim.g.zelli_navigator_move_focus_or_tab = 1
+      -- vim.g.zellij_navigator_no_default_mappings = 1
+    end,
+  },
+}
+--[[
+return {
   {
     "jakewvincent/mkdnflow.nvim",
-    rocks = "luautf8",
     config = function()
       require("mkdnflow").setup()
     end,
   },
   {
     "SmiteshP/nvim-navic",
-    requires = "neovim/nvim-lspconfig",
+    dependencies = { "neovim/nvim-lspconfig" },
   },
   {
     "anuvyklack/hydra.nvim",
-    config = function() end,
-  },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "marilari88/neotest-vitest",
-    },
-    config = function()
-      require("neotest").setup({
-        adapters = {
-          require("neotest-vitest"),
-        },
-      })
-    end,
   },
   {
     "jbyuki/venn.nvim",
-    requires = "anuvyklack/hydra.nvim",
+    dependencies = { "anuvyklack/hydra.nvim" },
     config = function()
       local Hydra = require("hydra")
       local hint = [[
- Arrow^^^^^^   Select region with <C-v> 
+ Arrow^^^^^^   Select region with <C-v>
  ^ ^ _K_ ^ ^   _f_: surround it with box
  _H_ ^ ^ _L_
  ^ ^ _J_ ^ ^                      _<Esc>_
-]]
+--]]
+-- Hydra({
+--  name = "Draw Diagram",
+--  hint = hint,
+--  config = {
+--   color = "pink",
+--   invoke_on_body = true,
+--   hint = {
+--     border = "rounded",
+--   },
+--   on_enter = function()
+--    vim.o.virtualedit = "all"
+--   end,
+--  },
+-- mode = "n",
+-- body = "<leader>D",
+-- heads = {
+--   { "H",     "<C-v>h:VBox<CR>" },
+--   { "J",     "<C-v>j:VBox<CR>" },
+--   { "K",     "<C-v>k:VBox<CR>" },
+--   { "L",     "<C-v>l:VBox<CR>" },
+--    { "f",     ":VBox<CR>",      { mode = "v" } },
+--     { "<Esc>", nil,              { exit = true } },
+--    },
+--   })
+--  end,
+-- },
 
-      Hydra({
-        name = "Draw Diagram",
-        hint = hint,
-        config = {
-          color = "pink",
-          invoke_on_body = true,
-          hint = {
-            border = "rounded",
-          },
-          on_enter = function()
-            vim.o.virtualedit = "all"
-          end,
-        },
-        mode = "n",
-        body = "<leader>d",
-        heads = {
-          { "H", "<C-v>h:VBox<CR>" },
-          { "J", "<C-v>j:VBox<CR>" },
-          { "K", "<C-v>k:VBox<CR>" },
-          { "L", "<C-v>l:VBox<CR>" },
-          { "f", ":VBox<CR>", { mode = "v" } },
-          { "<Esc>", nil, { exit = true } },
-        },
-      })
-    end,
-  },
-}
+--}
+--]]
